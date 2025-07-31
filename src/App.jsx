1cf1c74a42d2f4936eaa5077efe8e4925c206303
Tijil Minocha <tijilminocha05@gmail.com>
2025-07-31 23:29:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,8 @@ function App() {
 
   return (
     <>
-        <Navbar /> 
-        <div className="bg-slate-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-
-       <Manager/> 
-        </div>
-       <Footer/>
+        <div>APP UNDER MAINTAINENCE</div>
+        <div>Sorry for the inconvinience :</div>
     </>
   )
 }
