3f2a658793346e0a0ca86d7393a2f8e9338f5441
Tijil Minocha <tijilminocha05@gmail.com>
2025-07-31 23:31:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react' 
+import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
 
-function App() { 
+function App() {
 
   return (
     <>
-        <div>APP UNDER MAINTAINENCE</div>
-        <div>Sorry for the inconvinience :</div>
+      <Navbar />
+      <div className="bg-slate-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+
+        <Manager />
+      </div>
+      <Footer />
+      {/* <div>APP UNDER MAINTAINENCE</div>
+        <div>Sorry for the inconvenience :</div> */}
     </>
   )
 }
